35b74918a14e02e95b66f2c1f98e559309bcb33a
Nitish <mail.logicninjax@gmail.com>
2025-06-08 16:05:52 +0530

    add api request/response class

---
+++ src/main/java/com/cms/complaint_management_system/dto/api_response/GeneralResponse.java
@@ -0,0 +1,50 @@
+package com.cms.complaint_management_system.dto.api_response;
+
+import java.time.LocalDateTime;
+
+public class GeneralResponse<T> {
+
+    private Boolean success;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+
+    public GeneralResponse(Boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
