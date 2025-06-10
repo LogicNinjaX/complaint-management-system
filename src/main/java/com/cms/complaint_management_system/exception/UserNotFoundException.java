5b16db37cb9504f6e1b31dc916f68154018b07df
Nitish <mail.logicninjax@gmail.com>
2025-06-10 20:17:27 +0530

    add custom exception

---
+++ src/main/java/com/cms/complaint_management_system/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cms.complaint_management_system.exception;
+
+public class UserNotFoundException extends UserException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
