026bc4c6009a07ef5402676e35a3af0df7029801
Nitish <mail.logicninjax@gmail.com>
2025-06-11 14:21:27 +0530

    add custom query method

--- src/main/java/com/cms/complaint_management_system/repository/UserRepository.java
+++ src/main/java/com/cms/complaint_management_system/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.cms.complaint_management_system.repository;
 import com.cms.complaint_management_system.entity.UserRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserRecord, UUID> {
+
+    Optional<UserRecord> findByUsername(String username);
 }
