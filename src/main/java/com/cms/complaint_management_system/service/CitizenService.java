eec6bb74b70d87e50881c80ab6172f6b975c3512
Nitish <mail.logicninjax@gmail.com>
2025-06-12 21:17:30 +0530

    add service interface for citizen

---
+++ src/main/java/com/cms/complaint_management_system/service/CitizenService.java
@@ -0,0 +1,19 @@
+package com.cms.complaint_management_system.service;
+
+import com.cms.complaint_management_system.dto.CitizenDto;
+import com.cms.complaint_management_system.dto.api_request.CitizenUpdateRequest;
+import com.cms.complaint_management_system.entity.UserRecord;
+import com.cms.complaint_management_system.exception.UserNotFoundException;
+
+import java.util.UUID;
+
+public interface CitizenService {
+
+    CitizenDto saveCitizenDetails(UserRecord userRecord);
+
+    CitizenDto getCitizenDetails(UUID citizenId) throws UserNotFoundException;
+
+    void deleteCitizenDetails(UUID citizenId);
+
+    CitizenDto updateCitizenDetails(UUID citizenId, CitizenUpdateRequest request);
+}
