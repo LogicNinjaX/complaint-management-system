eeb368808d371dd9e4bcf87e413d2aeb12325b64
Nitish <mail.logicninjax@gmail.com>
2025-06-12 21:21:05 +0530

    update validation

--- src/main/java/com/cms/complaint_management_system/dto/api_request/UserLoginRequest.java
+++ src/main/java/com/cms/complaint_management_system/dto/api_request/UserLoginRequest.java
@@ -1,16 +1,13 @@
 package com.cms.complaint_management_system.dto.api_request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 
 public class UserLoginRequest {
 
     @NotEmpty(message = "required field")
-    @Size(min = 5, max = 30, message = "username must be between 5-30 characters")
     private String username;
 
     @NotEmpty(message = "required field")
-    @Size(min = 8, message = "password must be 8 characters long or more")
     private String password;
 
     public String getUsername() {
