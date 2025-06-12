90bca4a626d05eda0ec5532d1459f51337b2c748
Nitish <mail.logicninjax@gmail.com>
2025-06-12 21:19:12 +0530

    update service methods

--- src/main/java/com/cms/complaint_management_system/service/OfficerService.java
+++ src/main/java/com/cms/complaint_management_system/service/OfficerService.java
@@ -1,14 +1,22 @@
 package com.cms.complaint_management_system.service;
 
+import com.cms.complaint_management_system.dto.OfficerDto;
 import com.cms.complaint_management_system.dto.api_request.OfficerRegisterRequest;
 import com.cms.complaint_management_system.dto.api_request.OfficerUpdateRequest;
 import com.cms.complaint_management_system.entity.UserRecord;
+import com.cms.complaint_management_system.exception.DepartmentNotFoundException;
+import com.cms.complaint_management_system.exception.UserNotFoundException;
 
 import java.util.UUID;
 
 public interface OfficerService {
 
-    UserRecord saveOfficerDetails(OfficerRegisterRequest request);
+    OfficerDto saveOfficerDetails(OfficerRegisterRequest request);
 
-    UserRecord updateOfficerDetails(UUID officerId, OfficerUpdateRequest request);
+    OfficerDto getOfficerDetails(UUID officerId) throws UserNotFoundException;
+
+    void deleteOfficerDetails(UUID officerId);
+
+    UserRecord updateOfficerDetails(UUID officerId, OfficerUpdateRequest request) throws UserNotFoundException, DepartmentNotFoundException;
+
 }
