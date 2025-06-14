eed288e6d54e6b744fcbc66d524f4bf695964f6d
Nitish <mail.logicninjax@gmail.com>
2025-06-14 13:18:43 +0530

    add cache annotation

--- src/main/java/com/cms/complaint_management_system/ComplaintManagementSystemApplication.java
+++ src/main/java/com/cms/complaint_management_system/ComplaintManagementSystemApplication.java
@@ -2,10 +2,12 @@ package com.cms.complaint_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableMethodSecurity
+@EnableCaching
 public class ComplaintManagementSystemApplication {
 
 	public static void main(String[] args) {
