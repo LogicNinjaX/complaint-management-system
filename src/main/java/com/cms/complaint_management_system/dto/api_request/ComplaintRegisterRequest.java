a27df5a65ba56311c15ab4cc6bba2fefd8c4c0d7
Nitish <mail.logicninjax@gmail.com>
2025-06-15 13:37:22 +0530

    add api request class

---
+++ src/main/java/com/cms/complaint_management_system/dto/api_request/ComplaintRegisterRequest.java
@@ -0,0 +1,44 @@
+package com.cms.complaint_management_system.dto.api_request;
+
+public class ComplaintRegisterRequest {
+
+    private long categoryId;
+
+    private String title;
+
+    private String description;
+
+    private String address;
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
