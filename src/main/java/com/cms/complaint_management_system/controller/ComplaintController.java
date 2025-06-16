9bd6a3079abcd30df7475f05750b9bbe3290721d
Nitish <mail.logicninjax@gmail.com>
2025-06-16 09:26:35 +0530

    add rest endpoints

--- src/main/java/com/cms/complaint_management_system/controller/ComplaintController.java
+++ src/main/java/com/cms/complaint_management_system/controller/ComplaintController.java
@@ -9,10 +9,12 @@ import com.cms.complaint_management_system.security.CustomUserDetails;
 import com.cms.complaint_management_system.service.ComplaintService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -24,6 +26,7 @@ public class ComplaintController {
         this.complaintService = complaintService;
     }
 
+    // Role -> Citizen
     @PostMapping("/complaints")
     public ResponseEntity<GeneralResponse<ComplaintDto>> registerComplaint(@RequestBody ComplaintRegisterRequest request, @AuthenticationPrincipal CustomUserDetails user){
         var complaint = complaintService.registerComplaint(user.getUserId(), request);
@@ -31,6 +34,7 @@ public class ComplaintController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new GeneralResponse<>(true, "Complaint has been successfully registered", complaint));
     }
 
+    @PreAuthorize("hasAnyRole('OFFICER', 'ADMIN')")
     @GetMapping("/complaints")
     public ResponseEntity<GeneralResponse<List<ComplaintWithoutUserDto>>> getComplaints(
             @RequestParam(defaultValue = "1", required = false, name = "page") int pageNumber,
@@ -55,6 +59,59 @@ public class ComplaintController {
                 .body(new GeneralResponse<>(true, "Complaints fetched successfully", complaintList));
     }
 
+    @PreAuthorize("hasRole('CITIZEN')")
+    @GetMapping("/complaints/my")
+    public ResponseEntity<GeneralResponse<List<ComplaintWithoutUserDto>>> getMyComplaints(
+            @RequestParam(defaultValue = "1", required = false, name = "page") int pageNumber,
+            @RequestParam(defaultValue = "10", required = false, name = "size") int pageSize,
+            @RequestParam(defaultValue = "createdAt", required = false) String sortBy,
+            @RequestParam(defaultValue = "asc", required = false) String sortDir,
+            @AuthenticationPrincipal CustomUserDetails user
+    )
+    {
+        var complaintList = complaintService.getComplainsByUserId(user.getUserId(), pageNumber, pageSize, sortBy, sortDir);
 
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GeneralResponse<>(true, "Complaints fetched successfully", complaintList));
+    }
+
+    @PreAuthorize("hasAnyRole('OFFICER', 'ADMIN')")
+    @GetMapping("/complaints/{complaint-id}")
+    public ResponseEntity<GeneralResponse<?>> getByComplaintById(@PathVariable("complaint-id") UUID complaintId){
+        var complaint = complaintService.getComplainByComplaintId(complaintId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GeneralResponse<>(true, "Complaint fetched successfully", complaint));
+    }
+
+
+    @PreAuthorize("hasAnyRole('OFFICER', 'ADMIN')")
+    @PutMapping("/complaints/{complaint-id}/status")
+    public ResponseEntity<GeneralResponse<?>> updateComplaintStatus(
+            @PathVariable("complaint-id") UUID complaintId,
+            @RequestParam String status)
+    {
+
+        ComplaintStatus complaintStatus;
+        try {
+            complaintStatus = ComplaintStatus.valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid complaint status: " + status);
+        }
+
+        complaintService.updateComplaintStatus(complaintId, complaintStatus);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GeneralResponse<>(true, "Status updated successfully", null));
+    }
+
+
+    @PreAuthorize("hasAnyRole('OFFICER', 'ADMIN')")
+    @DeleteMapping("/complaints/{complaint-id}")
+    public ResponseEntity<GeneralResponse<?>> deleteComplaint(@PathVariable("complaint-id") UUID complaintId){
+        complaintService.deleteComplaintByComplaintId(complaintId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GeneralResponse<>(true, "Complaint deleted successfully", null));
+    }
 
 }
