a5dafcc3b98a32ab027e03c7761d3efa21cc3002
Nitish <mail.logicninjax@gmail.com>
2025-06-16 21:35:02 +0530

    add custom query method for deletion

--- src/main/java/com/cms/complaint_management_system/repository/DepartmentRepository.java
+++ src/main/java/com/cms/complaint_management_system/repository/DepartmentRepository.java
@@ -1,7 +1,15 @@
 package com.cms.complaint_management_system.repository;
 
 import com.cms.complaint_management_system.entity.Departments;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DepartmentRepository extends JpaRepository<Departments, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Departments d WHERE d.departmentId = :departmentId")
+    void deleteDepartmentById(long departmentId);
 }
