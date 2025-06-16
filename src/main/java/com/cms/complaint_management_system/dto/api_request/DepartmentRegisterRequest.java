df3dca27e7dbe9b866acce0791c186cbb26a8dc0
Nitish <mail.logicninjax@gmail.com>
2025-06-16 21:35:23 +0530

    add api request class

---
+++ src/main/java/com/cms/complaint_management_system/dto/api_request/DepartmentRegisterRequest.java
@@ -0,0 +1,24 @@
+package com.cms.complaint_management_system.dto.api_request;
+
+public class DepartmentRegisterRequest {
+
+    private long departmentId;
+
+    private String departmentName;
+
+    public long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
