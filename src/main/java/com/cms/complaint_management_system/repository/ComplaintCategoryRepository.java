c1a7a9c3eed56e831f07a9c5fde9001be890ef7d
Nitish <mail.logicninjax@gmail.com>
2025-06-17 11:21:04 +0530

    add custom query methods

--- src/main/java/com/cms/complaint_management_system/repository/ComplaintCategoryRepository.java
+++ src/main/java/com/cms/complaint_management_system/repository/ComplaintCategoryRepository.java
@@ -1,7 +1,21 @@
 package com.cms.complaint_management_system.repository;
 
 import com.cms.complaint_management_system.entity.ComplaintCategories;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ComplaintCategoryRepository extends JpaRepository<ComplaintCategories, Long> {
+
+
+    @Query("SELECT c FROM ComplaintCategories c WHERE c.departmentId = :departmentId")
+    Page<ComplaintCategories> getComplaintCategoriesByDepartmentId(long departmentId, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ComplaintCategories c WHERE c.categoryId = :categoryId")
+    void deleteCategoryById(long categoryId);
 }
