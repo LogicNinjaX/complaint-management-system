4121ab7d9bf728307856989c6fe15c3b8acbc9d1
Nitish <mail.logicninjax@gmail.com>
2025-06-18 09:49:05 +0530

    update handler methods

--- src/main/java/com/cms/complaint_management_system/controller/GlobalExceptionHandler.java
+++ src/main/java/com/cms/complaint_management_system/controller/GlobalExceptionHandler.java
@@ -2,9 +2,7 @@ package com.cms.complaint_management_system.controller;
 
 
 import com.cms.complaint_management_system.dto.api_response.ErrorResponse;
-import com.cms.complaint_management_system.exception.DepartmentNotFoundException;
-import com.cms.complaint_management_system.exception.OfficerException;
-import com.cms.complaint_management_system.exception.UserNotFoundException;
+import com.cms.complaint_management_system.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,14 +15,14 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler({UserNotFoundException.class, DepartmentNotFoundException.class})
+    @ExceptionHandler({UserNotFoundException.class, DepartmentNotFoundException.class, ComplaintNotFoundException.class, CategoryNotFoundException.class})
     public ResponseEntity<ErrorResponse<?>> userExceptionHandler(Exception e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse<>(false, e.getMessage(), HttpStatus.NOT_FOUND.value(), null));
     }
 
 
-    @ExceptionHandler(OfficerException.class)
+    @ExceptionHandler({OfficerException.class, CitizenException.class, CategoryException.class, DepartmentException.class})
     public ResponseEntity<ErrorResponse<?>> officerExceptionHandler(OfficerException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse<>(false, e.getMessage(), HttpStatus.BAD_REQUEST.value(), null));
