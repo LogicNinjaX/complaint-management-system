e236a9b52eccf2494540340f5144441a3d2954e9
Nitish <mail.logicninjax@gmail.com>
2025-06-21 20:31:36 +0530

    enable async

--- src/main/java/com/cms/complaint_management_system/ComplaintManagementSystemApplication.java
+++ src/main/java/com/cms/complaint_management_system/ComplaintManagementSystemApplication.java
@@ -3,11 +3,13 @@ package com.cms.complaint_management_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableMethodSecurity
 @EnableCaching
+@EnableAsync
 public class ComplaintManagementSystemApplication {
 
 	public static void main(String[] args) {
