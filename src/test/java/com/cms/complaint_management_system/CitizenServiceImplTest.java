cf09d65037fd63a7cf4962e91aaa0d03aba7b394
Nitish <mail.logicninjax@gmail.com>
2025-06-30 19:33:11 +0530

    Update CLass

--- src/test/java/com/cms/complaint_management_system/CitizenServiceImplTest.java
+++ src/test/java/com/cms/complaint_management_system/CitizenServiceImplTest.java
@@ -35,7 +35,7 @@ public class CitizenServiceImplTest {
 
     @InjectMocks
     private CitizenServiceImpl citizenService;
-
+    
     private UUID citizenId;
 
     private CitizenDto mockCitizenDto;
